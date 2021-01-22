fd6bf009ec017674ff4bd85269b0bfa1a1575309
Angelos-89 <fragiadoulakisaggelos@gmail.com>
2021-01-22 08:41:18 +0100

    Commit 04: Changed the setup.csh and setup.sh as well as the makefile so that it is easy to work with my laptop and the nordita server. 	modified:   HOWTO.run 	modified:   bin/README 	modified:   bin/setup.csh 	modified:   bin/setup.sh 	modified:   src/makefile

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,4 +5,4 @@ cd src
 ln -s /home/angelos/GIT/membrane_project/src/*.cpp .
 ln -s /home/angelos/GIT/membrane_project/src/*.hpp .
 ln -s /home/angelos/GIT/membrane_project/src/makefile .
-ln -s /home/angelos/GIT/membrane_project/config/config.angelos .
+ln -s /home/angelos/GIT/membrane_project/src/PARAMS .
