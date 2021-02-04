8a4167be04bcbfd0c7f38eec629b197511c32f9e
Angelos-89 <fragiadoulakisaggelos@gmail.com>
2021-02-04 16:32:30 +0100

    Commit 10: a) I changed the HOWTO.run to include instructions for parallel simulations. b) Modified the matlab script to plot histograms and calculate means and stds and also check for sample size. c) Changed to makefile so that the executable is called membrane.exe 	modified:   HOWTO.run 	modified:   matlab/plot_sim_results.m 	modified:   src/makefile

--- matlab/plot_sim_results.m
+++ matlab/plot_sim_results.m
@@ -1,11 +1,12 @@
 %% import data
-height = h5read("hfield.h5","/RectMesh");
-sampling = importdata("sampling.txt");
+samples = LoadSamples("sampling0.txt");
+height  = h5read("hfield0.h5","/RectMesh");
+height  = inner(height,2);
 
 %% lists of data
-area = sampling(:,1);
-alpha = sampling(:,3);
-energy = sampling(:,8);
+area   = samples(:,1);
+alpha  = samples(:,3);
+energy = samples(:,8);
  
 %% plot data
 figure(1)
@@ -33,3 +34,55 @@ plot(alpha);
 title("Lattice spacing vs MC steps");
 xlabel("MC steps");
 ylabel("alpha (a_0)");
+
+%% calculate means and stds and plot histograms
+begin = 2*1e4;
+
+area = area(begin:end);
+area_mean = mean(area);
+area_std = std(area);
+area_pd = normpdf(area,area_mean,area_std)/100;
+figure(5)
+histfit(area)
+title("Area per degrees of freedom");
+xlabel("Area per degrees of freedom (a_0^2/N^2)");
+ylabel("Occurances");
+grid on
+
+alpha = alpha(begin:end);
+alpha_mean = mean(alpha);
+alpha_std = std(alpha);
+alpha_pd = normpdf(alpha,alpha_mean,alpha_std)/100;
+figure(6)
+histfit(alpha);
+title("Lattice spacing");
+xlabel("Lattice spacing (a_0)");
+ylabel("Occurances");
+grid on
+
+energy = energy(begin:end);
+energy_mean = mean(energy);
+energy_std = std(energy);
+energy_pd = normpdf(energy,energy_mean,energy_std)/100;
+figure(7)
+histfit(energy)
+title("Energy per degrees of freedom");
+xlabel("Energy per degree of freedom (k_bT/N^2)");
+ylabel("Occurances");
+grid on
+
+%% define functions
+function samples = LoadSamples(filename)
+samples = importdata(filename); 
+    if length(samples(:,1)) < 1e4
+        error("Samples must be more than 1e4. Exiting.")
+    end
+end
+
+function field = inner(hfield,nghost)
+    field = hfield'; 
+    field(1:nghost,:)         = [];
+    field(end-nghost+1:end,:) = [];
+    field(:,1:nghost)         = [];
+    field(:,end-nghost+1:end) = [];
+end
