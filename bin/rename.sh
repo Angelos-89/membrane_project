a5f32fb05e5af68a8e09921a82e3f44cba88de24
angelos-89 <fragiadoulakisaggelos@gmail.com>
2021-04-27 14:50:22 +0200

    Added a bash script that renames the files hfield_rank.h5 to hfield_eq_rank.h5

---
+++ bin/rename.sh
@@ -0,0 +1,11 @@
+#used to rename the files hfield_0.h5 to hfield_eq_0.h5
+#so that we can start a simulation from an earlier one.
+#uncomment the following and change the directory according
+#the .h5 files location.
+#---------------------------------------#
+
+#FP="/home/angelos-89/bash_scripting"
+#for file in $FP/*.h5
+#do
+#    mv $file ${file%.h5}_eq.h5
+#done
