05851c0356bf7a53d87dfd308ebeefb987dc679d
angelos-89 <fragiadoulakisaggelos@gmail.com>
2021-05-12 09:41:31 +0200

    script to plot the pinned sites with different colors

---
+++ matlab/plot_pinned_sites_distribution.m
@@ -0,0 +1,75 @@
+%-------------------------------------------------------------------------%
+TITLENAMES = ["8% pinning","16% pinning","32% pinning"];
+FILENAMES = ["pinned_set_0.txt","pinned_set_1.txt","pinned_set_2.txt"];
+gridsPerDim = 80; %read this from PARAMS.txt
+%--------------------------Data to be plotted-----------------------------%
+Zs = {};
+for m=1:length(FILENAMES) 
+    pinnedSites = importdata(FILENAMES(m));
+    dataLen = size(pinnedSites,1);
+    xCoor = pinnedSites(:,1);
+    yCoor = pinnedSites(:,2);
+    xx = 0:1:gridsPerDim-1; yy = xx;
+    [X,Y] = meshgrid(xx,yy);
+    Z = zeros(gridsPerDim, gridsPerDim);
+    for k = 1:dataLen
+        i = xCoor(k);
+        j = yCoor(k);
+        Z(j+1,i+1) = 1; % i,j might be zero
+    end 
+    Zs{m} = Z;
+end
+%----------------------------------Now plot-------------------------------%
+fig = figure( 'Units', 'normalized', 'Position', [0.0, 0.0, 0.3, 0.5], ...
+        'Color', 'white' ) ;
+% - Bg axes.
+bgAxes = axes( 'Position', [0, 0, 1, 1], 'XColor', 'none', 'YColor', ...
+               'none', 'XLim', [0, 1], 'YLim', [0, 1] ) ;
+% - Positions.
+x = linspace( 0.05, 0.6, 4 ) ;
+w = 0.8 * diff( x(1:2) ) ;
+y = linspace( 0.07, 0.7, 3 ) ;
+h = 0.8 * diff( y(1:2) ) ;
+
+% - Surface 1
+axes( 'Position', [0.1, 0.58, 0.35, 0.35] ) ;
+s = surf(X,Y,Z) ;
+s.EdgeColor = 'none';
+xlim([0,gridsPerDim-1]);
+ylim([0,gridsPerDim-1]);
+view(2)
+ylabel("y (a_0)")
+title(TITLENAMES(1))
+
+% - Surface 2
+axes( 'Position', [0.55, 0.58, 0.35, 0.35] ) ;
+s = surf(X,Y,Z) ;
+s.EdgeColor = 'none';
+xlim([0,gridsPerDim-1]);
+ylim([0,gridsPerDim-1]);
+view(2)
+title(TITLENAMES(2))
+
+% - Surface 3
+axes( 'Position', [0.1, 0.15, 0.35, 0.35] ) ;
+s = surf(X,Y,Z) ;
+s.EdgeColor = 'none';
+xlim([0,gridsPerDim-1]);
+ylim([0,gridsPerDim-1]);
+view(2)
+title(TITLENAMES(3))
+xlabel("x (a_0)")
+ylabel("y (a_0)")
+
+% - Surface 4
+axes( 'Position', [0.55, 0.15, 0.35, 0.35] ) ;
+s = surf(X,Y,Z) ;
+s.EdgeColor = 'none';
+xlim([0,gridsPerDim-1]);
+ylim([0,gridsPerDim-1]);
+view(2)
+title(TITLENAMES(3))
+xlabel("x (a_0)")
+
+set( gca, 'Box', 'off' ) ;
+saveas(fig,"pinned-distribution.jpg");
